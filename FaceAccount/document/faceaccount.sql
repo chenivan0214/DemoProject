b196a16aa849208b171cb8c7d6e9e196e47d0cc1
chenivan0214 <chenjiahau@snailhouse.com.tw>
2016-09-19 11:35:37 +0800

    Feature: FaceAccount draft sql

---
+++ FaceAccount/document/faceaccount.sql
@@ -0,0 +1,16 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50713
+Source Host           : localhost:3306
+Source Database       : faceaccount
+
+Target Server Type    : MYSQL
+Target Server Version : 50713
+File Encoding         : 65001
+
+Date: 2016-09-19 11:35:14
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
